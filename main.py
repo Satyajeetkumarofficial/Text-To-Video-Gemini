983941f28c71e53c17515197586f7f1dc3f53f49
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-08-26 07:32:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ from pyrogram import Client, filters
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive", 200
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run).start()
 
 # Load .env file
 load_dotenv()
